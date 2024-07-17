04b995126fdba0b5f651b1cb183252b5913345a9
Ayisha-6238 <ayishaashfak425@gmail.com>
2024-07-17 11:43:33 +0530

    first commit

---
+++ git/pattern.js
@@ -0,0 +1,92 @@
+
+// var row=5
+// for (let i=row;i>=1;i--){
+//     var star=''
+//     for(let j=1;j<=i;j++){
+//         star = '*'+star
+       
+//     }
+//     console.log(star)
+// }
+  
+// for(let i=row;i>=1;i--){
+//     var ptrn=''
+//     for(let j=1;j<=row-i;j++){
+//         ptrn+=" "
+//     }
+//     for(let k=1;k<=i;k++){
+//         ptrn+="*"
+//     }
+//     console.log(ptrn)
+
+// }
+
+// // upper triangle
+// let n = 5;
+// for (let i = 1; i <= n; i++) {
+//     let str = "* ";
+//     console.log(str.repeat(i));
+// }
+
+// upper right triangle
+// let n = 5;
+// for (let i = 1; i <= n; i++) {
+//     let str = "* ";
+//     let space  = '  ';
+//     console.log(space.repeat((n-i))+str.repeat(i));
+// }
+
+
+// let n = 5; 
+// for (let i = 1; i <= n; i++) { 
+// 	let str = "* "; 
+// 	let space = ' '; 
+// 	console.log(space.repeat((n - i)) + str.repeat(i * 2 - 1)); 
+// }
+
+// var row=5;
+// for(i=1;i<=row;i++){
+//     var pattern=''
+//     for (j=1;j<=row-i;j++){
+//         // pattern=""+pattern
+//         pattern+=""
+//     }
+//         for(k=1;k<=i;k++){
+//         //   pattern=pattern+"*"
+//             pattern+="*"
+
+//         }
+//     console.log(pattern)
+// }
+
+
+// for(i=1;i<=row;i++){
+//     var pattern=''
+//     for (j=i;j<=row;j++){
+//         // pattern=""+pattern
+//         pattern+=""
+//     }
+//     console.log(pattern)
+// }
+// for(i=2;i<=row;i++){
+//     var pattern=''
+//     for (j=1;j<=i;j++){
+//         // pattern=""+pattern
+//         pattern+=""
+//     }
+//     console.log(pattern)
+// }
+
+
+var row=4
+ for (let i=row;i>=1;i--){
+     var star=''
+    for(let j=1;j<=i;j++){
+       star = '*'+star
+       
+   }
+   console.log(star)
+}
+ for(let i=1;i>=row-1;i++){
+  var space=" "
+for( for)
